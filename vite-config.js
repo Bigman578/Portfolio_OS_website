66d08efff22dcd9cb5c54cbd5f45b24bca29b0bf
Bwar <bwar62@gmail.com>
2024-04-24 11:31:24 +0200

    vite config change & more

--- vite-config.js
+++ vite-config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/Operator-System',
+    base: '/Operator-System/',
     plugins: [react()],
 })
