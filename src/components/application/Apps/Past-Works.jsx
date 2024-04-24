fb45f446ade2517af0107bd736d50ad9e1972501
Bwar <bwar62@gmail.com>
2024-04-24 11:15:13 +0200

    Update website file

--- src/components/application/Apps/Past-Works.jsx
+++ src/components/application/Apps/Past-Works.jsx
@@ -7,14 +7,14 @@ function Previouswork() {
             <div className="main-prevWork">
                 <div className="introduction-prevWork">
                     <h3>All projects</h3>
-                    <p>Compiled with date,company and final product</p>
+                    <p>Compiled with date, company and final product</p>
                 </div>
                 <div className="allProjects">
                     <div className="prevWork-display">
                         <h4>Project_NetonNet</h4>
-                        <p>A project focused entierly on recreating NetonNet to the ability of my classmates. In this project I was the Project leader for an extended duration working on it for 6 months. (This project has been altered because of React problems, the issue is still being looked into until a more better solution is found)</p>
-                        <p>2023-2024</p>
-                        <button onClick={() => {window.open('/Past_Projects/Project_NetonNet/index.html')}}>Open project in new tab</button>
+                        <p>A project focused entierly on recreating NetonNet to the ability of me and my classmates. <br/>In this project I was the Project leader for an extended duration working on it for 6 months. Combined this is our work within the time limit. </p>
+                        <p style={{fontStyle: 'italic'}}>2023-2024</p>
+                        <button onClick={() => {window.open('https://raufchaudhry.github.io/project_netonnet/')}}>Open project in new tab</button>
                     </div>
                 </div>
             </div>
