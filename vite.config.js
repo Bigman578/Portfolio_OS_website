ad0649e98f58ca4c3602615fb3b5a759030dbc30
Bwar <bwar62@gmail.com>
2024-04-24 14:46:22 +0200

    UPDATE vite.config update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: "/Portfolio_OS_website/", 
-    resolve: {
-        extensions: [
-            '.js',
-            '.json',
-            '.jsx'
-        ],
-    },
+    esbuild: {
+        loader: 'jsx',
+      },
+      resolve: {
+        alias: {
+          './runtimeConfig': './runtimeConfig.browser',
+        },
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+          loader: {
+            '.js': 'jsx',
+          },
+        },
+      },
 })
