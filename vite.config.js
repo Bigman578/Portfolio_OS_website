481acc4c41af2ed746907d04334956ab42ca0245
Bwar <bwar62@gmail.com>
2024-04-25 11:05:29 +0200

    UPDATE vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "./Portfolio_OS_website/", 
+    base: "/", 
     esbuild: {
         loader: 'jsx',
       },
