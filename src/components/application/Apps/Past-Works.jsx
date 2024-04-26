af2f457159fbd8846d343dcc3735c30af5bd8b3f
Bwar <bwar62@gmail.com>
2024-04-26 14:28:02 +0200

    quick change

--- src/components/application/Apps/Past-Works.jsx
+++ src/components/application/Apps/Past-Works.jsx
@@ -7,13 +7,13 @@ function Previousproject() {
             <div className="main-prevWork">
                 <div className="introduction-prevWork">
                     <h3>All projects</h3>
-                    <p>Compiled with date, company and final product</p>
+                    <p>Any bigger project that is not planned by myself compiled with date, ordering individual and final product</p>
                 </div>
                 <div className="allProjects">
                     <div className="prevWork-display">
                         <h4>Project_NetonNet</h4>
                         <p>A project focused entierly on recreating NetonNet to the ability of me and my classmates. <br/>In this project I was the Project leader for an extended duration working on it for 6 months. Combined this is our work within the time limit. </p>
-                        <p style={{fontStyle: 'italic'}}>2023-2024</p>
+                        <p style={{fontStyle: 'italic'}}>2023-2024, Sankta Marie Skolan</p>
                         <button onClick={() => {window.open('https://raufchaudhry.github.io/project_netonnet/')}}>Open project in new tab</button>
                     </div>
                 </div>
